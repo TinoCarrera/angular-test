94f30e7c9845290e937f0861851c4c632257cf08
Florentino Carrera <florentinocarrera44@gmail.com>
2025-03-21 18:45:35 +0100

    Implementar¡ndo el servicio de canciones en el componente Home y agregando lógica para obtener datos al iniciar.

--- src/app/pages/songs/home/home.component.ts
+++ src/app/pages/songs/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { SongsService } from '../../../services/songs.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  private songsService = inject(SongsService);
 
+  ngOnInit() {
+    this.songsService.getAll().subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
