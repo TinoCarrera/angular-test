5f9a610af76ddc25a4af39bf54daa3be29d22a99
Florentino Carrera <florentinocarrera44@gmail.com>
2025-03-21 18:17:27 +0100

    Agregando rutas y componente para la página de canciones.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'songs',
+    loadChildren: () => import('./pages/songs/songs.routes').then((m) => m.routes)
+  },
+  {
+    path: '',
+    redirectTo: 'songs',
+    pathMatch: 'full'
+  },
+];
